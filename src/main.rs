30180e637fedb962621619e45eb86101ba2aa2b6
rostislav <rostislav@mokuton.ru>
2023-04-08 12:12:59 +0800

    wip: impl cmd exec stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Write;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("echo")
+        .arg("Hello world")
+        .output()
+        .expect("Failed to execute command");
+
+    let mut out = std::io::stdout();
+    out.write_all(output.stdout.as_slice())
+        .expect("Failed to write to stdout");
 }
